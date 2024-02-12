84addc08495142f88d07fdf764e171e7b909c1d3
Jakub Rakoczy <office@rakoczy.io>
2024-02-12 01:10:44 +0100

    Add paging HN results

--- src/crawler/hackernews_crawler.py
+++ src/crawler/hackernews_crawler.py
@@ -4,52 +4,64 @@ from utils import fail_gracefully
 from bs4 import BeautifulSoup
 from typing import List, Optional
 from requests.models import Response
+from urllib.parse import urljoin
+
 
 SITE_LINK: str = "https://news.ycombinator.com/"
 LIST_ELEMENT_CLASS: str = "titleline"
 
 
 def articles() -> List[str]:
-    response: Response = get_with_retries(url=SITE_LINK)
-    soup: BeautifulSoup = BeautifulSoup(response.content, "html.parser")
-    urls: List[str] = _article_urls(soup=soup)
+    articles: List[str] = []
 
-    articles: List[str] = [
-        text
-        for article_link in urls
-        if (text := _single_article_text(article_link)) is not None
-    ]
+    page_link: Optional[str] = SITE_LINK
+    while page_link:
+        response: Response = get_with_retries(url=page_link)
+        soup: BeautifulSoup = BeautifulSoup(response.content, "html.parser")
+        urls: List[str] = _article_urls(soup=soup)
+
+        articles.extend(_article_texts(urls))
+
+        page_link = _next_page_link(soup=soup)
 
     return articles
 
 
 def _article_urls(soup: BeautifulSoup) -> List[str]:
-    list_elements: List[BeautifulSoup] = soup.find_all(
+    list_items: List[BeautifulSoup] = soup.find_all(
         "span", class_=LIST_ELEMENT_CLASS
     )
     urls: List[str] = [
-        found["href"]
-        for element in list_elements
-        if (found := element.find("a"))
+        found
+        for element in list_items
+        if element.find("a") and (found := element.find("a").get("href"))
+    ]
+    urls = [
+        url if url.startswith("http") else urljoin(SITE_LINK, url)
+        for url in urls
     ]
     return urls
 
 
-def _single_article_text(article_link: str) -> Optional[str]:
-    response: Optional[Response] = _send_request(article_link)
-    return _retrieve_text(response) if response is not None else None
+def _article_texts(urls: List[str]) -> List[str]:
+    article_texts: List[str] = []
+    for url in urls:
+        response: Optional[Response] = _send_request(url)
+        if response is not None:
+            article_texts.append(_retrieve_text(response))
+    return article_texts
 
 
-def _send_request(article_link: str) -> Optional[Response]:
-    url: str = (
-        SITE_LINK + article_link
-        if not article_link.startswith("http")
-        else article_link
-    )
-    article_response: Response = fail_gracefully(lambda: get_with_retries(url))
-    return article_response
+def _send_request(url: str) -> Optional[Response]:
+    response: Response = fail_gracefully(lambda: get_with_retries(url))
+    return response
 
 
-def _retrieve_text(response: Response) -> Optional[str]:
+def _retrieve_text(response: Response) -> str:
     soup: BeautifulSoup = BeautifulSoup(response.content, "html.parser")
     return fail_gracefully(soup.get_text)
+
+
+def _next_page_link(soup: BeautifulSoup) -> Optional[str]:
+    more_link: Optional[BeautifulSoup] = soup.find("a", text="More")
+    return urljoin(SITE_LINK, more_link["href"]) if more_link else None
