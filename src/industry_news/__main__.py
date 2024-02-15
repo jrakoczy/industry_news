e7bfea29a50aeeb2fbb44277c6271c369e86d63a
Jakub Rakoczy <office@rakoczy.io>
2024-02-15 23:38:35 +0100

    Switch to poetry

--- src/industry_news/__main__.py
+++ src/industry_news/__main__.py
@@ -3,10 +3,14 @@ from typing import List
 from industry_news.crawler.hackernews_crawler import articles
 from datetime import datetime, timedelta
 
-
 logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
+
+def main():
     since: datetime = datetime.now() - timedelta(hours=2)
     results: List[str] = articles(since=since)
     print(len(results))
+
+
+if __name__ == "__main__":
+    main()
